5eb875d9d74ea5aec47d9bf04011cb0814e0b437
StefanyCandida <stefany.candida@gmail.com>
2022-02-07 12:08:25 -0300

    inserindo projeto3

---
+++ src/App.js
@@ -0,0 +1,29 @@
+import React, { Component }  from 'react'
+import  fruta from ''
+
+class DadosPessoais extends Component {
+  state = {
+    name:`Stéfany Farias`,
+    idade :32,
+    comida :`sushi`,
+    musica: [`Sia - Chandelier `,`Sia - Cheap Thrills`,`Nando Reis - Relicário`]
+  }
+  render(){
+    return(
+      <div>
+        <h1>{this.state.name}</h1>
+        <h2>{this.state.idade}</h2>
+        <h3>{this.state.comida}</h3>
+        <h3>Comidas Favoritas</h3>
+        <ul>
+          <li>{this.state.musica[0]}</li>
+          <li>{this.state.musica[1]}</li>
+          <li>{this.state.musica[2]}</li>
+        </ul>
+        <img src={fruta}/>
+      </div>
+    );
+  }
+}
+
+export default DadosPessoais;
